1fa4a8b532c5471b2ddc441596362d399629ab5a
alexis <alexis_ceres@hotmail.com>
2021-12-17 20:01:02 -0500

    Included validations for register passwords. Firebase is used as user DB.

--- MarketEats/MarketEats/ViewControllers/LoginView.swift
+++ MarketEats/MarketEats/ViewControllers/LoginView.swift
@@ -9,7 +9,6 @@ import UIKit
 
 class LoginView: UIViewController {
     
-    
     @IBOutlet weak var emailText: UITextField!
     @IBOutlet weak var passwordText: UITextField!
     
@@ -19,6 +18,7 @@ class LoginView: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    //login button
     @IBAction func isPressed(_ sender: UIButton) {
         /*
          validate if password is accurate
@@ -29,6 +29,26 @@ class LoginView: UIViewController {
          
          if successful, login into home screen
          */
+        
+        if (((emailText.text?.isEmpty) != nil) || ((passwordText.text?.isEmpty) != nil)) {
+            let alert = UIAlertController.init(title: "Login Error" , message: "Please fill the entire form", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title:"OK", style:.default))
+            self.present(alert ,animated: true, completion: nil)
+        }
+        /*
+        else if(password is not in database or accurate){
+             let alert = UIAlertController.init(title: "Password Error" , message: "Password Incorrect", preferredStyle: .alert)
+             alert.addAction(UIAlertAction(title:"OK", style:.default))
+             self.present(alert ,animated: true, completion: nil)
+        }
+         
+         else if(password is not in database or accurate){
+              let alert = UIAlertController.init(title: "Email Error" , message: "Email Incorrect", preferredStyle: .alert)
+              alert.addAction(UIAlertAction(title:"OK", style:.default))
+              self.present(alert ,animated: true, completion: nil)
+         }
+        */
+
     }
     
     @IBAction func forgetPassword(_ sender: UIButton) {
@@ -36,3 +56,32 @@ class LoginView: UIViewController {
     }
     
 }
+
+
+/* OPTIONAL FEATURE
+ //passwordless authentication
+ let actionCodeSettings = ActionCodeSettings()
+ actionCodeSettings.url = URL(string: "marketeats-a6fe5.firebaseapp.com")
+ 
+ // The sign-in operation has to always be completed in the app.
+ actionCodeSettings.handleCodeInApp = true
+ actionCodeSettings.setIOSBundleID(Bundle.main.bundleIdentifier!)
+ actionCodeSettings.setAndroidPackageName("com.alexispowell.MarketEats",
+                                          installIfNotAvailable: false, minimumVersion: "12")
+ 
+ //sending authorization link
+ Auth.auth().sendSignInLink(toEmail: email,
+                            actionCodeSettings: actionCodeSettings) { error in
+   // ...
+     if let error = error {
+       self.showMessagePrompt(error.localizedDescription)
+       return
+     }
+     // The link was successfully sent. Inform the user.
+     // Save the email locally so you don't need to ask the user for it again
+     // if they open the link on the same device.
+     UserDefaults.standard.set(email, forKey: "Email")
+     self.showMessagePrompt("Check your email for link")
+     // ...
+ }
+ */
--- MarketEats/MarketEats/ViewControllers/RegisterView.swift
+++ MarketEats/MarketEats/ViewControllers/RegisterView.swift
@@ -7,6 +7,10 @@
 
 import UIKit
 
+import Firebase
+import FirebaseAuth
+
+
 class RegisterView: UIViewController {
 
     @IBOutlet weak var emailString: UITextField!
@@ -19,15 +23,49 @@ class RegisterView: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    
+    
 
-    @IBAction func isPressed(_ sender: UIButton) {
+    //submit button - Validations work
+    //create user
+    @IBAction func isPressed(_ sender: Any) {
+        
         //validate if email already exists
-        //forget password
+        //validate if fields are completed
+        if (((emailString.text?.isEmpty)! || (passwordText.text?.isEmpty)! || (passwordTextConfirm.text?.isEmpty)!)) {
+            
+            let alert = UIAlertController.init(title: "Registration Error" , message: "Please fill the entire form", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title:"OK", style:.default))
+            present(alert ,animated: true, completion: nil)
+          
+        }
+        
         //validate if both passwords are equal
-        //re-enter
+        else if( ((passwordText.text?.elementsEqual(passwordTextConfirm.text!))!) != true){
+            let alert = UIAlertController.init(title: "Password Confirmation Error" , message: "Passwords must match", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title:"OK", style:.default))
+            present(alert ,animated: true, completion: nil)
+
+        }
         
         //if successful, reroute to login
         //add user information in db
         
+        Auth.auth().createUser(withEmail: emailString.text!, password: passwordText.text!){ authResult,error in
+            
+        }
+        
+        
+        //success message
+        self.dismiss(animated: true, completion: nil) //dismiss page after registering
+        let alert = UIAlertController.init(title: "Success!" , message: "Registration Completed", preferredStyle: .alert)
+   
+        let vc = self.storyboard?.instantiateViewController(identifier: "LoginViewController") as! LoginView
+        
+        self.present(vc, animated: true, completion: nil)
+       
     }
+    
+    
+    
 }
